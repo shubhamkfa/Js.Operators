a1a13f306de0cda6fac2aa17226c06a39db1b6c6
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-02-20 19:08:16 +0530

    Add files via upload

---
+++ Arithmati Operator.js
@@ -0,0 +1,59 @@
+// let a=5;
+// let b=10;
+
+// console.log(a+b);
+// console.log("a=",a,"& b =",b);
+// console.log("a+b=",a+b)
+// console.log("a-b=",a-b)
+// console.log("a*b=",a*b)
+// console.log("a/b=",a/b)
+// console.log("a%b=",a%b)
+
+
+//Unary Operators
+// console.log("++a=",++a)
+// console.log(a)
+
+// console.log("a++=",a++)
+// console.log(a)
+// console.log("--a=",--a)
+// console.log(a)
+// console.log("a--=",a--)
+// console.log(a)
+
+
+
+//Assignment Operator
+// let a=5;
+// let b=2;
+// // a+=4;
+// // a-=2;
+// // a*=4;
+// // a/=4;
+// // a%=4;
+// a**=4;
+
+// console.log(a);
+
+//Comparison Operators
+
+// Equal to ==
+// Equal to type ===
+// not equal to !=
+// not equal to type !==
+
+let a=5;
+let b=10;
+
+// console.log(a==='5')
+// console.log(a=='5')
+// console.log(a===3)
+// console.log(a=='3')
+// console.log(b!='5')
+console.log(b!==11)
+console.log(a!=b)
+
+
+
+
+
